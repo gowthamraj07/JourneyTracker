3a7134234ea7d9e7ac82bc633d608c6395453aeb
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2024-03-08 22:04:27 +0100

    Add top bar to the StartJourneyScreen

--- app/src/main/java/com/gowthamraj07/journeytracker/ui/start/journey/StartJourneyScreen.kt
+++ app/src/main/java/com/gowthamraj07/journeytracker/ui/start/journey/StartJourneyScreen.kt
@@ -2,15 +2,20 @@ package com.gowthamraj07.journeytracker.ui.start.journey
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,16 +24,39 @@ import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 fun StartJourneyScreen(
     navigator: DestinationsNavigator
 ) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Start Journey",
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                },
+                modifier = Modifier.shadow(4.dp)
+            )
+        }
+    ) { paddingValues ->
+        StartJourneyContent(paddingValues, navigator)
+    }
+}
+
+@Composable
+private fun StartJourneyContent(paddingValues: PaddingValues, navigator: DestinationsNavigator) {
     Column(
         modifier = Modifier
             .background(color = MaterialTheme.colorScheme.surface)
             .fillMaxSize()
+            .padding(paddingValues)
             .padding(16.dp)
+
 
     ) {
         Text(
