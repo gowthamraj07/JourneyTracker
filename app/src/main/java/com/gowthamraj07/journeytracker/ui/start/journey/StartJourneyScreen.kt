3f5b0de8cfbf403aae143e8f5edc47395fb68bb3
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2024-03-11 00:35:47 +0100

    Request location permission to continue the flow

--- app/src/main/java/com/gowthamraj07/journeytracker/ui/start/journey/StartJourneyScreen.kt
+++ app/src/main/java/com/gowthamraj07/journeytracker/ui/start/journey/StartJourneyScreen.kt
@@ -1,5 +1,8 @@
 package com.gowthamraj07.journeytracker.ui.start.journey
 
+import android.Manifest
+import android.content.Intent
+import android.provider.Settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -22,9 +25,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.core.net.toUri
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberPermissionState
 import com.gowthamraj07.journeytracker.ui.destinations.OngoingJourneyScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -36,6 +46,9 @@ import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 fun StartJourneyScreen(
     navigator: DestinationsNavigator
 ) {
+
+    HandleLocationPermission()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -56,6 +69,90 @@ fun StartJourneyScreen(
         }
     ) { paddingValues ->
         StartJourneyContent(paddingValues, navigator)
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun HandleLocationPermission() {
+    val locationPermissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
+    when(locationPermissionState.status) {
+        is PermissionStatus.Denied -> {
+            if((locationPermissionState.status as PermissionStatus.Denied).shouldShowRationale) {
+                ShowDialogToOpenSettingsScreenToEnablePermission()
+            } else {
+                ShowDialogExplainWhyPermissionIsNeeded(locationPermissionState)
+            }
+        }
+        PermissionStatus.Granted -> {
+            // Do nothing
+        }
+    }
+
+}
+
+@Composable
+fun ShowDialogToOpenSettingsScreenToEnablePermission() {
+    val context = LocalContext.current
+
+    Dialog(onDismissRequest = { /*Do Nothing*/ }) {
+        Column(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.surface)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Location permission is needed to track your journey",
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentSize(align = Alignment.Center)
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Button(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                onClick = {
+                    // Open settings screen
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                        data = "package:${context.packageName}".toUri()
+                    }
+                    context.startActivity(intent)
+                }
+            ) {
+                Text(text = "Open Settings")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun ShowDialogExplainWhyPermissionIsNeeded(locationPermissionState: PermissionState) {
+    Dialog(onDismissRequest = { /*Do Nothing*/ }) {
+        Column(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.surface)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Location permission is needed to track your journey",
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentSize(align = Alignment.Center)
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Button(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                onClick = {
+                    locationPermissionState.launchPermissionRequest()
+                }
+            ) {
+                Text(text = "Request Permission")
+            }
+        }
     }
 }
 
