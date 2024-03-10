2f9d6f9ae6d28b21e1c9cfa853ed52dc2c199e44
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2024-03-10 13:17:50 +0100

    Emit state as Empty when there are no trips

--- app/src/test/java/com/gowthamraj07/journeytracker/ui/trips/TripsViewModelTest.kt
+++ app/src/test/java/com/gowthamraj07/journeytracker/ui/trips/TripsViewModelTest.kt
@@ -2,11 +2,14 @@ package com.gowthamraj07.journeytracker.ui.trips
 
 import com.gowthamraj07.journeytracker.domain.usecase.GetTripsUseCase
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
@@ -32,5 +35,13 @@ class TripsViewModelTest: StringSpec({
         tripsViewModel.loadTrips()
 
         verify { getTripsUseCase.execute() }
+    }
+
+    "should emit state as Empty when there are no trips" {
+        coEvery { getTripsUseCase.execute() } returns flowOf(emptyList())
+
+        tripsViewModel.loadTrips()
+
+        tripsViewModel.state.value shouldBe TripsUiState.Empty
     }
 })
