9cba698c1105a5839b46437084643c4ea6c79529
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2024-03-11 22:30:28 +0100

    Fix failing UI tests

--- app/src/androidTest/java/com/gowthamraj07/journeytracker/ui/ongoing/journey/OngoingJourneyScreenKtTest.kt
+++ app/src/androidTest/java/com/gowthamraj07/journeytracker/ui/ongoing/journey/OngoingJourneyScreenKtTest.kt
@@ -2,8 +2,10 @@ package com.gowthamraj07.journeytracker.ui.ongoing.journey
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,11 +14,14 @@ class OngoingJourneyScreenKtTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    private val viewModel = mockk<OngoingJourneyViewModel>(relaxed = true)
+    private val viewModel = mockk<OngoingJourneyViewModel>{
+        coEvery { loadPlacesFor(any()) } returns Unit
+        coEvery { places } returns MutableStateFlow(OngoingJourneyUIState.Loading)
+    }
 
     @Test
     fun triggerViewModelMethod_toLoadPlaces() {
-        val tripId = 1
+        val tripId = 1L
         with(composeTestRule) {
             setContent {
                 OngoingJourneyScreen(
